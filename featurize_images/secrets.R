7ba6e7a36f44b6ede03ef0ff815cb9a87668e627
hsm207 <hsm207@users.noreply.github.com>
2019-06-17 21:23:18 +0800

    Delete unneeded files

--- featurize_images/secrets.R
+++
@@ -1,1 +0,0 @@
-storageKey = "WpJqUKKq+8dgOGIXNlubRVrLu6vdNArNW9sE+cAGdwss1ETSb3P9ihjcSbFBQitAMs7RX/avXtGAYRORhuhHZA==";
--- startup.sh
+++
@@ -1,8 +0,0 @@
-# Add RServer libraries to the R session config, but only after all libs are installed
-# sudo echo "r-libs-user=/data/mlserver/9.2.1/libraries/RServer" >>/etc/rstudio/rsession.conf
-
-# Enable the RStudio Server service
-sudo systemctl enable rstudio-server
-
-# Start the RStudio Server service
-sudo systemctl start rstudio-server
